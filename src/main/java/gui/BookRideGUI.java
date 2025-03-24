d2423d89bed2e1e6c9601192df13f536332f51cd
Ander <157314696+a-nder2@users.noreply.github.com>
2025-03-24 21:39:25 +0100

    Add files via upload

---
+++ src/main/java/gui/BookRideGUI.java
@@ -0,0 +1,345 @@
+package gui;
+
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.Rectangle;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+//import java.awt.event.WindowAdapter;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.text.DateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+import java.util.ResourceBundle;
+import java.util.Vector;
+
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+
+import com.toedter.calendar.JCalendar;
+
+import businessLogic.BLFacade;
+import configuration.UtilDate;
+import domain.Ride;
+import domain.Traveler;
+
+
+public class BookRideGUI extends JFrame {
+	private static final long serialVersionUID = 1L;
+
+
+	private JComboBox<String> jComboBoxOrigin = new JComboBox<String>();
+	DefaultComboBoxModel<String> originLocations = new DefaultComboBoxModel<String>();
+	private JButton jButtonClose = new JButton(ResourceBundle.getBundle("Etiquetas").getString("CreateRideGUI.Close"));
+	private JComboBox<String> jComboBoxDestination = new JComboBox<String>();
+	DefaultComboBoxModel<String> destinationCities = new DefaultComboBoxModel<String>();
+
+	private JLabel jLabelOrigin = new JLabel(ResourceBundle.getBundle("Etiquetas").getString("CreateRideGUI.LeavingFrom"));
+	private JLabel jLabelDestination = new JLabel(ResourceBundle.getBundle("Etiquetas").getString("CreateRideGUI.GoingTo"));
+	private final JLabel jLabelEventDate = new JLabel(ResourceBundle.getBundle("Etiquetas").getString("CreateRideGUI.RideDate"));
+	private final JLabel jLabelEvents = new JLabel(ResourceBundle.getBundle("Etiquetas").getString("CreateRideGUI.Rides")); 
+
+	private JButton jPayTravel = new JButton(ResourceBundle.getBundle("Etiquetas").getString("BookRideGUI.Payment"));
+
+	// Code for JCalendar
+	private JCalendar jCalendar1 = new JCalendar();
+	private Calendar calendarAnt = null;
+	private Calendar calendarAct = null;
+	private JScrollPane scrollPaneEvents = new JScrollPane();
+
+	private List<Date> datesWithRidesCurrentMonth = new Vector<Date>();
+
+	private JTable tableRides= new JTable();
+	private final JButton jClosi = new JButton(ResourceBundle.getBundle("Etiquetas").getString("Close"));
+	private DefaultTableModel tableModelRides;
+
+
+	private String[] columnNamesRides = new String[] {
+			ResourceBundle.getBundle("Etiquetas").getString("BookRideGUI.Driver"), 
+			ResourceBundle.getBundle("Etiquetas").getString("BookRideGUI.NPlaces"), 
+			ResourceBundle.getBundle("Etiquetas").getString("BookRideGUI.Price")
+	};
+	
+
+
+	public BookRideGUI(Traveler t)
+	{
+		//setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.getContentPane().setLayout(null);
+		this.setSize(new Dimension(700, 500));
+		this.setTitle(ResourceBundle.getBundle("Etiquetas").getString("BookRideGUI.FindRides"));
+
+		jLabelEventDate.setBounds(new Rectangle(457, 6, 140, 25));
+		jLabelEvents.setBounds(172, 229, 259, 16);
+
+		this.getContentPane().add(jLabelEventDate, null);
+		this.getContentPane().add(jLabelEvents);
+
+		jPayTravel.setBounds(new Rectangle(137, 420, 130, 30));
+
+		jPayTravel.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String salida = jComboBoxOrigin.getSelectedItem().toString();
+				String llegada = jComboBoxDestination.getSelectedItem().toString();
+				Date data = jCalendar1.getDate();
+				Ride r = null;
+				int fila = tableRides.getSelectedRow();
+				ArrayList<String> filaTexto = new ArrayList<String>();
+				if (fila >= 0 && fila < tableRides.getRowCount()) {
+					for (int columna = 0; columna < tableRides.getColumnCount(); columna++) {
+						filaTexto.add(tableRides.getValueAt(fila, columna).toString());
+					}
+					System.out.println("Fila " + fila + ": " + filaTexto.toString());
+				} else {
+					System.out.println("Indice de fila fuera de rango.");
+				}
+				if (!filaTexto.isEmpty()) {
+					r = ApplicationLauncher.da.findRides(filaTexto.get(0), salida, llegada, data,
+							(int) Float.parseFloat(filaTexto.get(1)), Float.parseFloat(filaTexto.get(2)));
+				} else {
+					System.out.println("ERROR Gravisimo en bookRideGUI, Requiere intervencion manual");
+				}
+				if (r != null) {
+					System.out.println("a bookRide llega esta persona" + r.toString());
+					PaymentGUI d = new PaymentGUI(t, r);
+					d.setVisible(true);
+				} else {
+					System.out.println("ERROR HAS HAPPENED");
+				}
+				jButton2_actionPerformed(e);
+			}
+		});
+
+		BLFacade facade = MainGUI.getBusinessLogic();
+		List<String> origins=facade.getDepartCities();
+		
+		for(String location:origins) originLocations.addElement(location);
+		
+		jLabelOrigin.setBounds(new Rectangle(6, 56, 92, 20));
+		jLabelDestination.setBounds(6, 81, 61, 16);
+		getContentPane().add(jLabelOrigin);
+
+		getContentPane().add(jLabelDestination);
+
+		jComboBoxOrigin.setModel(originLocations);
+		jComboBoxOrigin.setBounds(new Rectangle(103, 50, 172, 20));
+		
+
+		List<String> aCities=facade.getDestinationCities((String)jComboBoxOrigin.getSelectedItem());
+		for(String aciti:aCities) {
+			destinationCities.addElement(aciti);
+		}
+		
+		jComboBoxOrigin.addItemListener(new ItemListener() {
+			public void itemStateChanged(ItemEvent e) {
+				destinationCities.removeAllElements();
+				BLFacade facade = MainGUI.getBusinessLogic();
+
+				List<String> aCities=facade.getDestinationCities((String)jComboBoxOrigin.getSelectedItem());
+				for(String aciti:aCities) {
+					destinationCities.addElement(aciti);
+				}
+				tableModelRides.getDataVector().removeAllElements();
+				tableModelRides.fireTableDataChanged();
+
+				
+			}
+		});
+
+
+		jComboBoxDestination.setModel(destinationCities);
+		jComboBoxDestination.setBounds(new Rectangle(103, 80, 172, 20));
+		jComboBoxDestination.addItemListener(new ItemListener() {
+			public void itemStateChanged(ItemEvent e) {
+
+				paintDaysWithEvents(jCalendar1,datesWithRidesCurrentMonth,	new Color(210,228,238));
+
+				BLFacade facade = MainGUI.getBusinessLogic();
+
+				datesWithRidesCurrentMonth=facade.getThisMonthDatesWithRides((String)jComboBoxOrigin.getSelectedItem(),(String)jComboBoxDestination.getSelectedItem(),jCalendar1.getDate());
+				paintDaysWithEvents(jCalendar1,datesWithRidesCurrentMonth,Color.CYAN);
+
+			}
+		});
+
+		this.getContentPane().add(jPayTravel, null);
+		this.getContentPane().add(jComboBoxOrigin, null);
+
+		this.getContentPane().add(jComboBoxDestination, null);
+
+
+		jCalendar1.setBounds(new Rectangle(300, 50, 225, 150));
+
+
+		// Code for JCalendar
+		jCalendar1.addPropertyChangeListener(new PropertyChangeListener()
+		{
+			public void propertyChange(PropertyChangeEvent propertychangeevent)
+			{
+
+				if (propertychangeevent.getPropertyName().equals("locale"))
+				{
+					jCalendar1.setLocale((Locale) propertychangeevent.getNewValue());
+				}
+				else if (propertychangeevent.getPropertyName().equals("calendar"))
+				{
+					calendarAnt = (Calendar) propertychangeevent.getOldValue();
+					calendarAct = (Calendar) propertychangeevent.getNewValue();
+					
+
+					
+					DateFormat dateformat1 = DateFormat.getDateInstance(1, jCalendar1.getLocale());
+
+					int monthAnt = calendarAnt.get(Calendar.MONTH);
+					int monthAct = calendarAct.get(Calendar.MONTH);
+
+					if (monthAct!=monthAnt) {
+						if (monthAct==monthAnt+2) {
+							// Si en JCalendar está 30 de enero y se avanza al mes siguiente, devolvería 2 de marzo (se toma como equivalente a 30 de febrero)
+							// Con este código se dejará como 1 de febrero en el JCalendar
+							calendarAct.set(Calendar.MONTH, monthAnt+1);
+							calendarAct.set(Calendar.DAY_OF_MONTH, 1);
+						}
+
+						jCalendar1.setCalendar(calendarAct);
+
+					}
+					
+					try {
+						tableModelRides.setDataVector(null, columnNamesRides);
+						tableModelRides.setColumnCount(4); // another column added to allocate ride objects
+
+						BLFacade facade = MainGUI.getBusinessLogic();
+						List<domain.Ride> rides=facade.getRides((String)jComboBoxOrigin.getSelectedItem(),(String)jComboBoxDestination.getSelectedItem(),UtilDate.trim(jCalendar1.getDate()));
+
+						if (rides.isEmpty() ) jLabelEvents.setText(ResourceBundle.getBundle("Etiquetas").getString("BookRideGUI.NoRides")+ ": "+dateformat1.format(calendarAct.getTime()));
+						else jLabelEvents.setText(ResourceBundle.getBundle("Etiquetas").getString("BookRideGUI.Rides")+ ": "+dateformat1.format(calendarAct.getTime()));
+						for (domain.Ride ride:rides){
+							Vector<Object> row = new Vector<Object>();
+							row.add(ride.getDriver().getName());
+							row.add(ride.getnPlaces());
+							row.add(ride.getPrice());
+							row.add(ride); // ev object added in order to obtain it with tableModelEvents.getValueAt(i,3)
+							tableModelRides.addRow(row);		
+						}
+						datesWithRidesCurrentMonth=facade.getThisMonthDatesWithRides((String)jComboBoxOrigin.getSelectedItem(),(String)jComboBoxDestination.getSelectedItem(),jCalendar1.getDate());
+						paintDaysWithEvents(jCalendar1,datesWithRidesCurrentMonth,Color.CYAN);
+
+
+					} catch (Exception e1) {
+
+						e1.printStackTrace();
+					}
+					tableRides.getColumnModel().getColumn(0).setPreferredWidth(170);
+					tableRides.getColumnModel().getColumn(1).setPreferredWidth(30);
+					tableRides.getColumnModel().getColumn(1).setPreferredWidth(30);
+					tableRides.getColumnModel().removeColumn(tableRides.getColumnModel().getColumn(3)); // not shown in JTable
+
+				}
+			} 
+			
+		});
+
+		jButtonClose.setBounds(new Rectangle(148, 420, 130, 30));
+		jButtonClose.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+		});
+		
+		this.getContentPane().add(jCalendar1, null);
+
+		scrollPaneEvents.setBounds(new Rectangle(172, 257, 346, 150));
+
+		scrollPaneEvents.setViewportView(tableRides);
+		tableModelRides = new DefaultTableModel(null, columnNamesRides);
+
+		tableRides.setModel(tableModelRides);
+
+		tableModelRides.setDataVector(null, columnNamesRides);
+		tableModelRides.setColumnCount(4); // another column added to allocate ride objects
+
+		tableRides.getColumnModel().getColumn(0).setPreferredWidth(170);
+		tableRides.getColumnModel().getColumn(1).setPreferredWidth(30);
+		tableRides.getColumnModel().getColumn(1).setPreferredWidth(30);
+
+		tableRides.getColumnModel().removeColumn(tableRides.getColumnModel().getColumn(3)); // not shown in JTable
+
+		this.getContentPane().add(scrollPaneEvents, null);
+		datesWithRidesCurrentMonth=facade.getThisMonthDatesWithRides((String)jComboBoxOrigin.getSelectedItem(),(String)jComboBoxDestination.getSelectedItem(),jCalendar1.getDate());
+		paintDaysWithEvents(jCalendar1,datesWithRidesCurrentMonth,Color.CYAN);
+		jClosi.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				MainGUIt b = new MainGUIt(t);
+				b.setVisible(true);
+			}
+		});
+		jClosi.setBounds(new Rectangle(137, 420, 130, 30));
+		jClosi.setBounds(467, 424, 130, 30);
+		
+		getContentPane().add(jClosi);
+
+	}
+	@SuppressWarnings("deprecation")
+	public static void paintDaysWithEvents(JCalendar jCalendar,List<Date> datesWithEventsCurrentMonth, Color color) {
+		//		// For each day with events in current month, the background color for that day is changed to cyan.
+
+
+		Calendar calendar = jCalendar.getCalendar();
+
+		int month = calendar.get(Calendar.MONTH);
+		int today=calendar.get(Calendar.DAY_OF_MONTH);
+		int year=calendar.get(Calendar.YEAR);
+
+		calendar.set(Calendar.DAY_OF_MONTH, 1);
+		int offset = calendar.get(Calendar.DAY_OF_WEEK);
+
+		if (Locale.getDefault().equals(new Locale("es")))
+			offset += 4;
+		else
+			offset += 5;
+
+
+		for (Date d:datesWithEventsCurrentMonth){
+
+			calendar.setTime(d);
+
+
+			// Obtain the component of the day in the panel of the DayChooser of the
+			// JCalendar.
+			// The component is located after the decorator buttons of "Sun", "Mon",... or
+			// "Lun", "Mar"...,
+			// the empty days before day 1 of month, and all the days previous to each day.
+			// That number of components is calculated with "offset" and is different in
+			// English and Spanish
+			//			    		  Component o=(Component) jCalendar.getDayChooser().getDayPanel().getComponent(i+offset);; 
+			Component o = (Component) jCalendar.getDayChooser().getDayPanel()
+					.getComponent(calendar.get(Calendar.DAY_OF_MONTH) + offset);
+			o.setBackground(color);
+		}
+
+		calendar.set(Calendar.DAY_OF_MONTH, today);
+		calendar.set(Calendar.MONTH, month);
+		calendar.set(Calendar.YEAR, year);
+
+
+	}
+	
+	private void jButton2_actionPerformed(ActionEvent e) {
+		this.setVisible(false);
+	}
+
+}
